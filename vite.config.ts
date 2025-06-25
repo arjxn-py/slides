46c170f7cc4c09cffd8b52a966198cda62127baa
arjxn-py <arjxn.py@gmail.com>
2025-06-25 20:15:16 +0530

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite';
+import copy from 'rollup-plugin-copy';
 
 export default defineConfig({
   root: '.',
   server: {
     open: true,
   },
+  build: {
+    rollupOptions: {
+      plugins: [
+        copy({
+          targets: [
+            { src: 'slides.md', dest: 'dist' }
+          ],
+          hook: 'writeBundle'
+        })
+      ]
+    }
+  }
 });
