84029f00973b160320d70080980ec5119f547899
arjxn-py <arjxn.py@gmail.com>
2025-09-16 15:24:52 +0530

    Add gh pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import copy from 'rollup-plugin-copy';
 
 export default defineConfig({
   root: '.',
+  base: '/slides/',
   server: {
     open: true,
   },
   build: {
+    outDir: 'dist',
     rollupOptions: {
       plugins: [
         copy({
